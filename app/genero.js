859928cf46458b2ae12090285e057770d443a90f
SakayHT <amandiuxy@gmail.com>
2023-08-08 01:32:11 -0500

    generos

--- app/genero.js
+++ app/genero.js
@@ -1,105 +1,99 @@
-// ********* Género *******
+// *** Género ***
 function post() {
-    document.getElementById("myform").addEventListener("submit", function (event) {
-        event.preventDefault();
-        var datos = {
-            nombre: document.getElementById("nombre").value,
-            genero: document.getElementById("genero").value,
-        }
+  document.getElementById("myform").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var datos = {
+      nombre: document.getElementById("nombre").value,
+      genero: document.getElementById("genero").value,
+    }
 
-        fetch('http://127.0.0.1:8000/api/genres', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(datos)
-        })
-            .then(res => res.json())
-            .then(json => console.log(json))
+    fetch('http://127.0.0.1:8000/api/genres', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datos)
     })
+      .then(res => res.json())
+      .then(json => console.log(json))
+  })
 }
 
 
 function borrar(id) {
-    id = parseInt(id);
-    fetch('http://127.0.0.1:8000/api/genres' + id, {
-        method: "DELETE"
-    })
-        .then(res => res.json())
-        .then(json => console.log(json))
-    alert("datos eliminados");
+  id = parseInt(id);
+  fetch('http://127.0.0.1:8000/api/genres' + id, {
+    method: "DELETE"
+  })
+    .then(res => res.json())
+    .then(json => console.log(json))
+  alert("datos eliminados");
 
 }
 
 function obtenerDatos() {
-    var endPoint = document.getElementById("endpoint").value;
-    var tabla = document.getElementById("tbody");
-    fetch(endPoint)
-      .then(function (respuesta) {
-        return respuesta.json();
-      })
-      .then(function (datos) {
-        console.log(datos);
-        var contenido = "";
-        for (var i = 0; i < datos.length; i++) {
-          if (datos[i].name != undefined && datos[i].genre_id != undefined) {
-            contenido =
-              contenido +
-              "<tr><td>" +
-              datos[i].name +
-              "</td><td>" +
-              datos[i].genre_id +
-              "</td><td><button class='btn btn-warning'>Editar</button>" +
-              "<button class='btn btn-danger' onclick='borrar(" +
-              datos[i].id +
-              ")'>Eliminar</button></td></tr>";
-          }
+  var endPoint = document.getElementById("endpoint").value;
+  var tabla = document.getElementById("tbody");
+  fetch(endPoint)
+    .then(function (respuesta) {
+      return respuesta.json();
+    })
+    .then(function (datos) {
+      console.log(datos);
+      var contenido = "";
+      for (var i = 0; i < datos.length; i++) {
+        if (datos[i].name != undefined) {
+          contenido =
+            contenido +
+            "<tr><td>" +
+            datos[i].name +
+            "</td><td>" +
+            "</td><td><button class='btn btn-warning'>Editar</button>" +
+            "<button class='btn btn-danger' onclick='borrar(" +
+            datos[i].id +
+            ")'>Eliminar</button></td></tr>";
         }
-        tabla.innerHTML = contenido;
-        consumir();
-      });
-    }
-
-
-
-    function consumir() {
-        var endPoint = document.getElementById("endpoint").value;
-        var name = [];
-        var genre_id = [];
-      
-        // Llamado a la API
-        fetch(endPoint)
-          // Promesa cuando se cumple o cuando la respuesta es exitosa
-          .then(function (respuesta) {
-            return respuesta.json();
-          })
-          // Promesa recibe los datos en formato JSON
-          .then(function (datos) {
-            for (var i = 0; i < datos.length; i++) {
-              if (datos[i].name != undefined && datos[i].genre_id != undefined) {
-                name.push(datos[i].name);
-                genre_id.push(datos[i].genre_id);
-              }
-            }
-      
-            var data = [
-              {
-                x: name,
-                y: genre_id,
-                type: "bar",
-                orientation: "v",
-                marker: { color: "rgba(0,0,255)" },
-              },
-            ];
-      
-            var layout = {
-              title: "Gráfica de barras",
-            };
-      
-            Plotly.newPlot("myDiv1", data, layout);
-          });
       }
-    
+      tabla.innerHTML = contenido;
+      consumir();
+    });
+}
 
 
+
+function consumir() {
+  var endPoint = document.getElementById("endpoint").value;
+  var name = [];
   
+
+  // Llamado a la API
+  fetch(endPoint)
+    // Promesa cuando se cumple o cuando la respuesta es exitosa
+    .then(function (respuesta) {
+      return respuesta.json();
+    })
+    // Promesa recibe los datos en formato JSON
+    .then(function (datos) {
+      for (var i = 0; i < datos.length; i++) {
+        if (datos[i].name != undefined) {
+          name.push(datos[i].name);
+        }
+      }
+      //Grafica de burbujas
+      var trace1 = {
+        x: name,
+        mode: 'markers',
+        marker: {
+          size: [40, 60, 80, 100]
+        }
+      };
+      var data = [trace1];
+      var layout = {
+        title: 'Marker Size',
+        showlegend: false,
+        height: 600,
+        width: 600
+      };
+      Plotly.newPlot('myDiv4', data, layout);
+    })
+}
